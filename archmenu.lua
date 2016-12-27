5e94c52f87c3440d3f0a9ee2dbc75aa930feaeba
Jonathan Peel <me@jonathanpeel.co.za>
2016-12-27 19:03:50 +0200

    First Commit

---
+++ archmenu.lua
@@ -0,0 +1,24 @@
+ local menuc8205c7636e728d448c2774e6a4a944b = {
+     {"Avahi SSH Server Browser", "/usr/bin/bssh"},
+     {"Avahi VNC Server Browser", "/usr/bin/bvnc"},
+     {"Chromium", "chromium ", "/usr/share/icons/hicolor/16x16/apps/chromium.png" },
+     {"Firefox", "firefox ", "/usr/share/icons/hicolor/16x16/apps/firefox.png" },
+ }
+
+ local menu52dd1c847264a75f400961bfb4d1c849 = {
+     {"Qt V4L2 test Utility", "qv4l2", "/usr/share/icons/hicolor/16x16/apps/qv4l2.png" },
+ }
+
+ local menuee69799670a33f75d45c57d1d1cd0ab3 = {
+     {"Avahi Zeroconf Browser", "/usr/bin/avahi-discover"},
+     {"Termite", "termite"},
+     {"UXTerm", "uxterm", "/usr/share/pixmaps/xterm-color_48x48.xpm" },
+     {"XTerm", "xterm", "/usr/share/pixmaps/xterm-color_48x48.xpm" },
+ }
+
+xdgmenu = {
+    {"Internet", menuc8205c7636e728d448c2774e6a4a944b},
+    {"Sound & Video", menu52dd1c847264a75f400961bfb4d1c849},
+    {"System Tools", menuee69799670a33f75d45c57d1d1cd0ab3},
+}
+
