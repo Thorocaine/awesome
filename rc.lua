e97fe82c1a336c92468668c69a0ada74aef08030
Jonathan Peel <me@jonathanpeel.co.za>
2016-12-28 12:43:03 +0200

    update

--- rc.lua
+++ rc.lua
@@ -39,6 +39,7 @@ end
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
 beautiful.init("/usr/share/awesome/themes/zenburn/theme.lua")
+theme.wallpaper = "/home/jonathan/wallpaper/arch1.jpg"
 
 -- This is used later as the default terminal and editor to run.
 terminal = "termite"
